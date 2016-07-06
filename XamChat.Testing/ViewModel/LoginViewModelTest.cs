22d60c30c3fb88550be26c50a2b6ee7776beaffe
utherkrandt <ph_legrand@msn.com>
2016-07-06 09:23:53 +0200

    oubli?

---
+++ XamChat.Testing/ViewModel/LoginViewModelTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using XamChat.Service;
+using Infrastructure.ServiceLocator;
+using XamChat.ViewModels;
+using System.Threading.Tasks;
+
+namespace XamChat.Testing.ViewModel
+{
+	[TestFixture]
+	public class LoginViewModelTest
+	{
+		private ISetting setting;
+		private LoginViewModel loginViewModel;
+
+		[SetUp]
+		public void SetUp()
+		{
+			Test.SetUp();
+			setting = ServiceContainer.Instance.Resolve<ISetting>();
+			loginViewModel = new LoginViewModel();
+			                          
+		}
+
+		[Test]
+		public async Task LoginTest()
+		{
+			loginViewModel.Username = "TestUsername";
+			loginViewModel.Password = "TestPassword";
+
+			await loginViewModel.Login();
+
+			Assert.IsNotNull(setting.User);
+			Assert.AreEqual(loginViewModel.Username, setting.User.Username);
+			Assert.AreEqual(loginViewModel.Password, setting.User.Password);
+
+		}
+	}
+}
+
